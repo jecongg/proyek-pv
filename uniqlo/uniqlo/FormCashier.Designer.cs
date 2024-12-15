9df92f053a82a74ae7d40a10053529813c7384fc
treviss11 <trevisartagrantdy@gmail.com>
2024-12-15 13:37:46 +0700

    sempurnain

--- uniqlo/uniqlo/FormCashier.Designer.cs
+++ uniqlo/uniqlo/FormCashier.Designer.cs
@@ -33,15 +33,31 @@ namespace uniqlo
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonLogout
             // 
-            this.buttonLogout.Location = new System.Drawing.Point(1856, 31);
+            this.buttonLogout.Location = new System.Drawing.Point(2223, 37);
             this.buttonLogout.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.buttonLogout.Name = "buttonLogout";
-            this.buttonLogout.Size = new System.Drawing.Size(200, 69);
+            this.buttonLogout.Size = new System.Drawing.Size(178, 69);
             this.buttonLogout.TabIndex = 0;
             this.buttonLogout.Text = "LOGOUT";
             this.buttonLogout.UseVisualStyleBackColor = true;
@@ -49,19 +65,23 @@ namespace uniqlo
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(5, 133);
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(-5, 133);
             this.panel1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(2125, 931);
+            this.panel1.Size = new System.Drawing.Size(2427, 1026);
             this.panel1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(156, 42);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(153, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(303, 69);
+            this.label1.Size = new System.Drawing.Size(245, 55);
             this.label1.TabIndex = 2;
             this.label1.Text = "CASHIER";
             // 
@@ -73,11 +93,135 @@ namespace uniqlo
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(536, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 39);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Pengguna";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Pria",
+            "Wanita",
+            "Anak",
+            "Bayi"});
+            this.comboBox1.Location = new System.Drawing.Point(754, 53);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 39);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1016, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(230, 39);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Jenis Pakaian";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "T-Shirt",
+            "Sweat",
+            "Fleece",
+            "Kaos Polo",
+            "Kemeja",
+            "Bawahan",
+            "Luaran",
+            "Sweater Rajut",
+            "Dalaman",
+            "Loungewear",
+            "Aksesoris",
+            "Sport Utility Wear",
+            ""});
+            this.comboBox2.Location = new System.Drawing.Point(1262, 54);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(254, 39);
+            this.comboBox2.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1575, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 39);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Search";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1726, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 38);
+            this.textBox1.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2084, 37);
+            this.button1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 69);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "CART";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(50, 30);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(409, 457);
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(690, 30);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(409, 457);
+            this.pictureBox3.TabIndex = 16;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(1332, 30);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(409, 457);
+            this.pictureBox4.TabIndex = 17;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(1969, 30);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(409, 457);
+            this.pictureBox5.TabIndex = 18;
+            this.pictureBox5.TabStop = false;
+            // 
             // FormCashier
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2133, 1073);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(2418, 1159);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
@@ -86,7 +230,12 @@ namespace uniqlo
             this.Name = "FormCashier";
             this.Text = "FormCashier";
             this.Load += new System.EventHandler(this.FormCashier_Load);
+            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,5 +247,16 @@ namespace uniqlo
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
--- uniqlo/uniqlo/FormCashier.cs
+++ uniqlo/uniqlo/FormCashier.cs
@@ -37,24 +37,69 @@ namespace uniqlo
         private void FormCashier_Load(object sender, EventArgs e)
         {
             string imageUrl = "https://brandslogos.com/wp-content/uploads/images/large/uniqlo-logo.png";
-
+            string imageItem1 = "https://image.uniqlo.com/UQ/ST3/us/imagesgoods/462369/item/usgoods_15_462369.jpg";
+            string imageItem2 = "https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/433635/item/goods_23_433635.jpg?width=2000";
+            string imageItem3 = "https://tse2.mm.bing.net/th?id=OIP.mspf-7Hwrvn8YNTmGDgVywHaHa&pid=Api&P=0&h=180";
+            string imageItem4 = "https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/460331/sub/goods_460331_sub14.jpg?width=600";
             try
             {
                 using (WebClient client = new WebClient())
                 {
                     byte[] imageData = client.DownloadData(imageUrl);
+                    byte[] imageData2 = client.DownloadData(imageItem1);
+                    byte[] imageData3 = client.DownloadData(imageItem2);
+                    byte[] imageData4 = client.DownloadData(imageItem3);
+                    byte[] imageData5 = client.DownloadData(imageItem4);
 
                     using (var stream = new System.IO.MemoryStream(imageData))
                     {
                         pictureBox1.Image = Image.FromStream(stream);
                         pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
                     }
+
+                    using (var stream = new System.IO.MemoryStream(imageData2))
+                    {
+                        pictureBox2.Image = Image.FromStream(stream);
+                        pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+                    }
+
+                    using (var stream = new System.IO.MemoryStream(imageData3))
+                    {
+                        pictureBox3.Image = Image.FromStream(stream);
+                        pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+                    }
+
+                    using (var stream = new System.IO.MemoryStream(imageData4))
+                    {
+                        pictureBox4.Image = Image.FromStream(stream);
+                        pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+                    }
+
+                    using (var stream = new System.IO.MemoryStream(imageData5))
+                    {
+                        pictureBox5.Image = Image.FromStream(stream);
+                        pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+                    }
+
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error loading image: " + ex.Message);
             }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Hide();
+            FormCart f = new FormCart();
+            f.ShowDialog();
+            this.Show();
         }
     }
 }
